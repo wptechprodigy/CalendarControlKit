99697f852671d6e6d51c1c2b862852e66433aae9
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-05-08 09:15:39 +0100

    Remove unnecessary comments

--- Package.swift
+++ Package.swift
@@ -13,10 +13,6 @@ let package = Package(
             name: "CalendarControlKit",
             targets: ["CalendarControlKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         .binaryTarget(
             name: "CalendarControlKit",
